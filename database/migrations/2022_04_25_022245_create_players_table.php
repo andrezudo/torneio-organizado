3bea8ed8a8e64cde8c6a6bd41a3a2d2b9ad95b48
André Luis <aluis7649@gmail.com>
2022-04-24 23:37:37 -0300

    Criação da migration para jogadores

---
+++ database/migrations/2022_04_25_022245_create_players_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name");
+            $table->foreignId('team_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+        Schema::table('players', function (Blueprint $table) {
+            $table->foreignId('team_id')
+            ->constrained()
+            ->onDelete('cascade');
+        });
+    }
+}
