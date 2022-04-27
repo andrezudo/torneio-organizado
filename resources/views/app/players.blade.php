446f44852c249a4b6de1b797221d89aae2d1ec01
André Luis <aluis7649@gmail.com>
2022-04-27 01:03:48 -0300

    Listagem dos jogadores salvos no BD

--- resources/views/app/players.blade.php
+++ resources/views/app/players.blade.php
@@ -27,6 +27,42 @@ $rankings = '';
                   <div class="container text-white">
                       <h3>Jogadores:</h3>
                       <hr>
+
+                      @foreach ($players as $player)
+                        <div class="d-flex justify-content-between">
+                          <a type="button" data-bs-toggle="modal" data-bs-target="#palyerModalEdit"><i class="fa-solid fa-user"></i> {{$player->name}}</a>
+                          <a href="#" class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
+                        </div>
+                        <hr>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="palyerModalEdit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                          <div class="modal-dialog">
+                          <div class="modal-content">
+                              <div class="modal-header">
+                              <h5 class="modal-title" id="exampleModalLabel">Editar jogador</h5>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                              </div>
+                              <div class="modal-body">
+                                  <form action="/championship" method="POST">
+                                      @csrf
+                                      <div class="mb-3">
+                                        <label for="title" class="form-label">Jogador:</label>
+                                        <input type="text" class="form-control" id="title" name="title" placeholder="Nome do jogador">
+                                      </div>
+                                      <div class="modal-footer">
+                                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                                          <button type="submit" class="btn btn-primary">Salvar</button>
+                                      </div>
+                                  </form>
+                              </div>
+                          </div>
+                          </div>
+                      </div>
+
+                      @endforeach
+
+                      <!--
                       <div class="d-flex justify-content-between">
                         <a type="button" data-bs-toggle="modal" data-bs-target="#palyerModal"><i class="fa-solid fa-user"></i> Fulano</a>
                         <a href="#" class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
@@ -57,6 +93,7 @@ $rankings = '';
                         <a href="#" class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
                       </div>
                       <hr>
+                    -->
                   </div>
 
                   <!-- Modal -->
