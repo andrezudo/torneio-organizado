b893cb724f57d35ec0ced055781cba376aaf927c
André Luis <aluis7649@gmail.com>
2022-04-27 01:02:45 -0300

    Retornar jogadores por time

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Player;
+use App\Models\Team;
 
 class PlayerController extends Controller
 {
-    public function players() {   
+    /*public function players() {   
 
         return view('app.players');
+    }*/
+
+    public function players($id) {
+
+        $team = Team::findOrFail($id);
+        $players = $team->players;
+
+        return view('app.players', ['players' => $players]);
     }
 }
