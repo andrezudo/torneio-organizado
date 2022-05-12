9f0cc18bc10c7bea37d4c672e76b3bb6417edbba
André Luis <aluis7649@gmail.com>
2022-05-12 02:02:54 -0300

    Formulário e listagem de jogos #15

--- resources/views/app/jogos.blade.php
+++ resources/views/app/jogos.blade.php
@@ -11,26 +11,49 @@ $rankings = '';
 @section('title','Jogos')
     
 @section('content')
+
+                <div class="container">
+                    <div class="row mt-5 mb-4 text-white">
+                        <div class="d-flex justify-content-between mb-5">
+                            <h2>Jogos do campeonato</h2>
+                            <div>
+                                <!--
+                                <a type="button" href="/app/gerar-jogos" class="btn btn-register">
+                                    <i class="fa-solid fa-plus"></i> Adicionar jogo
+                                </a>
+                                -->
+                                <button type="button" class="btn btn-register" data-bs-toggle="modal" data-bs-target="#gameModal">
+                                    <i class="fa-solid fa-plus"></i> Adicionar jogo
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
                 
-                  <div class="container mt-5 text-white">
+                <div class="container mt-5 text-white">  
 
                       <hr>
+                      @foreach ($games as $game)
+
                       <div class="text-center d-flex justify-content-evenly">
                           <div>
-                              <p>Time 1 <i class="fa-solid fa-shield-halved"></i></p>
+                              <p>{{$game->team1_id}} <i class="fa-solid fa-shield-halved"></i></p>
                           </div>
                           <div>
-                              <span><b>3</b> </span>
+                              <span><b>{{$game->team1_goals}}</b> </span>
                               <span> X </span>
-                              <span> <b>2</b></span>
+                              <span> <b>{{$game->team2_goals}}</b></span>
                           </div>
                           <div>
-                            <p><i class="fa-solid fa-shield-halved"></i> Time 1</p>
+                            <p><i class="fa-solid fa-shield-halved"></i> {{$game->team2_id}}</p>
                         </div>
                       </div>
                       <hr>
+                      
+                      @endforeach
 
-                      <div class="text-center d-flex justify-content-evenly">
+                    <!--
+                    <div class="text-center d-flex justify-content-evenly">
                         <div>
                             <p>Time 3 <i class="fa-solid fa-shield-halved"></i></p>
                         </div>
@@ -41,50 +64,65 @@ $rankings = '';
                         </div>
                         <div>
                           <p><i class="fa-solid fa-shield-halved"></i> Time 4</p>
-                      </div>
+                        </div>
                     </div>
                     <hr>
-
-                    <div class="text-center d-flex justify-content-evenly">
-                        <div>
-                            <p>Time 5 <i class="fa-solid fa-shield-halved"></i></p>
-                        </div>
-                        <div>
-                            <span><b>1</b> </span>
-                            <span> X </span>
-                            <span> <b>1</b></span>
-                        </div>
-                        <div>
-                          <p><i class="fa-solid fa-shield-halved"></i> Time 6</p>
-                      </div>
-                    </div>
-                    <hr>
+                    -->
                       
                   </div>
 
-                  <!-- Modal -->
-                  <div class="modal fade" id="palyerModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Adicionar jogador</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <form action="/championship" method="POST">
-                                @csrf
-                                <div class="mb-3">
-                                  <label for="title" class="form-label">Jogador:</label>
-                                  <input type="text" class="form-control" id="title" name="title" placeholder="Nome do jogador">
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                                    <button type="submit" class="btn btn-primary">Salvar</button>
-                                </div>
-                            </form>
-                        </div>
+                  <!-- Modal adicionar jogo-->
+                  <div class="modal fade" id="gameModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg">
+                      <div class="modal-content">
+                          <div class="modal-header">
+                          <h5 class="modal-title" id="exampleModalLabel">Adicionar jogo</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body">
+                              <form action="/app/game" method="POST">
+                                  @csrf
+                                  <div class="mb-3">
+                                    <input type="text" class="form-control" id="championship_id" name="championship_id" value="{{session('championship')->id}}" style="display: none;">
+                                  </div>
+                                  <div class="mb-3">
+                                    <div class="row">
+                                        <div class="col-sm-7">
+                                            <select id="team1_id" name="team1_id" class="form-select" aria-label="Default select example">
+                                                <option selected>Selecione o primeiro time</option>
+                                                @foreach ($teams as $team)
+                                                <option value="{{$team->id}}">{{$team->name}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="col-sm-5">
+                                            <input type="number" class="form-control" id="team1_goals" name="team1_goals" placeholder="Gols">
+                                        </div>
+                                    </div>  
+                                  </div>
+                                  <div class="mb-3">
+                                    <div class="row">
+                                        <div class="col-sm-7">
+                                            <select id="team2_id" name="team2_id" class="form-select" aria-label="Default select example">
+                                                <option selected>Selecione o primeiro time</option>
+                                                @foreach ($teams as $team)
+                                                <option value="{{$team->id}}">{{$team->name}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="col-sm-5">
+                                            <input type="number" class="form-control" id="team2_goals" name="team2_goals" placeholder="Gols">
+                                        </div>
+                                    </div>  
+                                  </div>
+                                  <div class="modal-footer">
+                                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                                      <button type="submit" class="btn btn-primary">Salvar</button>
+                                  </div>
+                              </form>
+                          </div>
+                      </div>
                     </div>
-                    </div>
-                </div>
+                  </div>
 
 @endsection
