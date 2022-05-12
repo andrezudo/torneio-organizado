7add8dadaa4caf7825f7df758983e994e3c9b07f
André Luis <aluis7649@gmail.com>
2022-05-12 02:01:22 -0300

    Model para jogos #15 #16 #17

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function team1() {
+        return $this->hasOne('App\Models\Team');
+    }
+    public function team2() {
+        return $this->hasOne(Team::class, 'team2_id');
+    }
+
 }
