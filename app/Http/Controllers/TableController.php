4c9da900551f808201da71f5300b6e2da2b9714c
André Luis <aluis7649@gmail.com>
2022-05-13 01:40:34 -0300

    Consulta da pontuação dos times #23

---
+++ app/Http/Controllers/TableController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Championship;
+use App\Models\Table;
+
+class TableController extends Controller
+{
+    public function tabela($id) {
+
+        //$championship = Championship::findOrFail($id);
+        //$tables = $championship->tables;
+        
+        $tables = Table::where('Championship_id', '=', $id)
+                    ->orderBy('points', 'desc')
+                    ->orderBy('victory', 'desc')
+                    ->orderBy('sg', 'desc')
+                    ->orderBy('id', 'asc')
+                    ->get();
+
+        return view('app.tabela', ['tables' => $tables]);
+    }
+}
