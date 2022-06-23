72a8c12d1c74fb2dbce0420c1d628c08445a71a3
André Luis <aluis7649@gmail.com>
2022-06-23 00:09:43 -0300

    Alteração para salvar dados da partida #20

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -15,32 +15,71 @@ class ResultController extends Controller
     public function store(Request $request) {
         
         $result = new Result;
+        if(isset($request->team1_goals)) {
+            $team1_goals = count($request->team1_goals);
+        }else {
+            $team1_goals = 0;
+        }
+        if(isset($request->team2_goals)) {
+            $team2_goals = count($request->team2_goals);
+        }else {
+            $team2_goals = 0;
+        }
+
+        if (isset($request->team1_yellowcard)) {
+            $team1_yellowcard = count($request->team1_yellowcard);
+        } else {
+            $team1_yellowcard = 0;
+        }
+        if (isset($request->team2_yellowcard)) {
+            $team2_yellowcard = count($request->team2_yellowcard);
+        } else {
+            $team2_yellowcard = 0;
+        }
+
+        if (isset($request->team1_redcard)) {
+            $team1_redcard = count($request->team1_redcard);
+        } else {
+            $team1_redcard = 0;
+        }
+        if (isset($request->team2_redcard)) {
+            $team2_redcard = count($request->team2_redcard);
+        } else {
+            $team2_redcard = 0;
+        }
+
+        /*
+        echo $team1_goals.' - '.$team2_goals.'<br>';
+        echo $team1_yellowcard.' - '.$team2_yellowcard.'<br>';
+        echo $team1_redcard.' - '.$team2_redcard;
+        */
+
         $result->championship_id = $request->championship_id;
         $result->game_id = $request->game_id;
-        $result->team1_goals = $request->team1_goals;
-        $result->team1_yellowcard = $request->team1_yellowcard;
-        $result->team1_redcard = $request->team1_redcard;
-        $result->team2_goals = $request->team2_goals;
-        $result->team2_yellowcard = $request->team2_yellowcard;
-        $result->team2_redcard = $request->team2_redcard;
+        $result->team1_goals = $team1_goals;
+        $result->team1_yellowcard = $team1_yellowcard;
+        $result->team1_redcard = $team1_redcard;
+        $result->team2_goals = $team2_goals;
+        $result->team2_yellowcard = $team2_yellowcard;
+        $result->team2_redcard = $team2_redcard;
         $result->save();
 
 
         $table1 = Table::where('team_id', '=', $request->team1_id)->firstOrFail();
         $table2 = Table::where('team_id', '=', $request->team2_id)->firstOrFail();
 
-        $table1->sg = $table1->sg + ($request->team1_goals - $request->team2_goals);
-        $table2->sg = $table2->sg + ($request->team2_goals - $request->team1_goals);
+        $table1->sg = $table1->sg + ($team1_goals - $team2_goals);
+        $table2->sg = $table2->sg + ($team2_goals - $team1_goals);
 
-        if( $request->team1_goals > $request->team2_goals){
+        if( $team1_goals > $team2_goals){
             $table1->points = $table1->points + 3;
             $table1->victory = $table1->victory + 1;
             $table2->defeat = $table2->defeat + 1;
-        }elseif($request->team1_goals < $request->team2_goals){
+        }elseif($team1_goals < $team2_goals){
             $table2->points = $table2->points + 3;
             $table2->victory = $table2->victory + 1;
             $table1->defeat = $table1->defeat + 1;
-        }elseif ($request->team1_goals == $request->team2_goals) {
+        }elseif ($team1_goals == $team2_goals) {
             $table1->points = $table1->points + 1;
             $table2->points = $table2->points + 1;
             $table2->draw = $table2->draw + 1;
