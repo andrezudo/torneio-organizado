bea865034ed82398af25d35eaf3b726747370392
André Luis <aluis7649@gmail.com>
2022-06-23 00:08:32 -0300

    Novo modal para adicinar resultado da partida #20

--- resources/views/app/jogos.blade.php
+++ resources/views/app/jogos.blade.php
@@ -93,6 +93,9 @@ $rankings = '';
                                     </button>
                                 @endif
                             @endif
+                            <button style="background: #8BC34A;border-color: #8BC34A;" type="button" class="btn btn-warning mx-2 my-2 btn-sm" data-bs-toggle="modal" data-bs-target="#resultTesteModal{{$game->id}}">
+                                <i class="fa-solid fa-triangle-exclamation"></i>
+                            </button>
                         </div>
                       </div>
                       <hr>
@@ -221,6 +224,252 @@ $rankings = '';
                         </div>
                         </div>
                       </div>
+
+                      <!-- Modal adicionar Resultado Teste-->
+                      <div class="modal fade" id="resultTesteModal{{$game->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                        <div class="modal-content text-black">
+                            <div class="modal-header">
+                            <h5 class="modal-title text-black" id="exampleModalLabel">Adicionar Resultado</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form action="/app/result" method="POST">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control" id="championship_id" name="championship_id" value="{{session('championship')->id}}" style="display: none;">
+                                        <input type="text" class="form-control" id="game_id" name="game_id" value="{{$game->id}}" style="display: none;">
+                                        <input type="text" class="form-control" id="team1_id" name="team1_id" value="{{$game->team1_id}}" style="display: none;">
+                                        <input type="text" class="form-control" id="team2_id" name="team2_id" value="{{$game->team2_id}}" style="display: none;">
+                                    </div>
+                                    <div class="mb-5">
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <h4>{{$game->team1->name}}</h4>
+                                            </div>
+                                            <div class="col-sm-4 container-gols-t1{{$game->id}}">
+                                                <label class="form-label">Gols</label> 
+                                                <a class="btn btn-sm btn-primary add_gol_t1{{$game->id}}"><i class="fa-solid fa-plus"></i></a>
+                                            </div>
+                                            <div class="col-sm-4 container-ca-t1{{$game->id}}">
+                                                <label class="form-label">Cartões amarelos</label>
+                                                <a class="btn btn-sm btn-primary add_ca_t1{{$game->id}}"><i class="fa-solid fa-plus"></i></a>
+                                            </div>
+                                            <div class="col-sm-4 container-cv-t1{{$game->id}}">
+                                                <label class="form-label">Cartões vermelhos</label>
+                                                <a class="btn btn-sm btn-primary add_cv_t1{{$game->id}}"><i class="fa-solid fa-plus"></i></a>
+                                            </div>
+                                        </div>  
+                                    </div>
+                                    <hr>
+                                    <div class="mb-3 mt-5">
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <h4>{{$game->team2->name}}</h4>
+                                            </div>
+                                            <div class="col-sm-4 container-gols-t2{{$game->id}}">
+                                                <label class="form-label">Gols</label> 
+                                                <a class="btn btn-sm btn-primary add_gol_t2{{$game->id}}"><i class="fa-solid fa-plus"></i></a>
+                                            </div>
+                                            <div class="col-sm-4 container-ca-t2{{$game->id}}">
+                                                <label class="form-label">Cartões amarelos</label>
+                                                <a class="btn btn-sm btn-primary add_ca_t2{{$game->id}}"><i class="fa-solid fa-plus"></i></a>
+                                            </div>
+                                            <div class="col-sm-4 container-cv-t2{{$game->id}}">
+                                                <label class="form-label">Cartões vermelhos</label>
+                                                <a class="btn btn-sm btn-primary add_cv_t2{{$game->id}}"><i class="fa-solid fa-plus"></i></a>
+                                            </div>
+                                        </div>  
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                                        <button type="submit" class="btn btn-primary">Salvar</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        </div>
+                      </div>
+
+                      <script>
+                        $(document).ready(function() {
+                      
+                            //adicionar gols time 1 
+                            var container_gols_t1 = $(".container-gols-t1{{$game->id}}");
+                            var add_button_gt1 = $(".add_gol_t1{{$game->id}}");
+                            var x_gt1 = 1;
+                            $(add_button_gt1).click(function(e) {
+                                e.preventDefault();
+                                x_gt1++;
+                                $(container_gols_t1).append(`
+                                  <div class="row mt-2">
+                                      <div class="col-10">
+                                          <select class="form-select" id="team1_goals" name="team1_goals[]" aria-label="Default select example">
+                                              <option selected value="futebol">Selecione o Jogador</option>
+                                              @foreach ( $game->team1->players as $playerr1)
+                                              <option value="{{$playerr1->id}}">{{$playerr1->name}}</option>
+                                              @endforeach
+                                          </select>
+                                      </div>
+                                      <a class="btn btn-sm btn-primary delete col-2"><i class="fa-solid fa-minus"></i></a>
+                                  </div>
+                                  `); //add input box
+                            });
+                      
+                            $(container_gols_t1).on("click", ".delete", function(e) {
+                                e.preventDefault();
+                                $(this).parent('div').remove();
+                                x_gt1--;
+                            })
+                            // fim add gols t1
+                      
+                            //adicionar cartões amarelos time 1
+                            var container_ca_t1 = $(".container-ca-t1{{$game->id}}");
+                            var add_button_cat1 = $(".add_ca_t1{{$game->id}}");
+                            var x_cat1 = 1;
+                            $(add_button_cat1).click(function(e) {
+                                e.preventDefault();
+                                x_cat1++;
+                                $(container_ca_t1).append(`
+                                  <div class="row mt-2">
+                                      <div class="col-10">
+                                          <select class="form-select" id="team1_yellowcard" name="team1_yellowcard[]" aria-label="Default select example">
+                                              <option selected value="futebol">Selecione o Jogador</option>
+                                              @foreach ( $game->team1->players as $playerr)
+                                              <option value="{{$playerr->id}}">{{$playerr->name}}</option>
+                                              @endforeach
+                                          </select>
+                                      </div>
+                                      <a class="btn btn-sm btn-primary delete col-2"><i class="fa-solid fa-minus"></i></a>
+                                  </div>
+                                  `); //add input box
+                            });
+                      
+                            $(container_ca_t1).on("click", ".delete", function(e) {
+                                e.preventDefault();
+                                $(this).parent('div').remove();
+                                x_cat1--;
+                            })
+                            // fim add ca t1
+                      
+                            //adicionar cartões vermelhos time 1
+                            var container_cv_t1 = $(".container-cv-t1{{$game->id}}");
+                            var add_button_cvt1 = $(".add_cv_t1{{$game->id}}");
+                            var x_cvt1 = 1;
+                            $(add_button_cvt1).click(function(e) {
+                                e.preventDefault();
+                                x_cvt1++;
+                                $(container_cv_t1).append(`
+                                  <div class="row mt-2">
+                                      <div class="col-10">
+                                          <select class="form-select" id="team1_redcard" name="team1_redcard[]" aria-label="Default select example">
+                                              <option selected value="futebol">Selecione o Jogador</option>
+                                              @foreach ( $game->team1->players as $playerr)
+                                              <option value="{{$playerr->id}}">{{$playerr->name}}</option>
+                                              @endforeach
+                                          </select>
+                                      </div>
+                                      <a class="btn btn-sm btn-primary delete col-2"><i class="fa-solid fa-minus"></i></a>
+                                  </div>
+                                  `); //add input box
+                            });
+                      
+                            $(container_cv_t1).on("click", ".delete", function(e) {
+                                e.preventDefault();
+                                $(this).parent('div').remove();
+                                x_cvt1--;
+                            })
+                            // fim add cv t1
+                      
+                            //adicionar gols time 2  
+                            var container_gols_t2 = $(".container-gols-t2{{$game->id}}");
+                            var add_button = $(".add_gol_t2{{$game->id}}");
+                            var x = 1;
+                            $(add_button).click(function(e) {
+                                e.preventDefault();
+                                x++;
+                                $(container_gols_t2).append(`
+                                  <div class="row mt-2">
+                                      <div class="col-10">
+                                          <select class="form-select" id="team2_goals" name="team2_goals[]" aria-label="Default select example">
+                                              <option selected value="futebol">Selecione o Jogador</option>
+                                              @foreach ( $game->team2->players as $playerr)
+                                              <option value="{{$playerr->id}}">{{$playerr->name}}</option>
+                                              @endforeach
+                                          </select>
+                                      </div>
+                                      <a class="btn btn-sm btn-primary delete col-2"><i class="fa-solid fa-minus"></i></a>
+                                  </div>
+                                  `); //add input box
+                            });
+                      
+                            $(container_gols_t2).on("click", ".delete", function(e) {
+                                e.preventDefault();
+                                $(this).parent('div').remove();
+                                x--;
+                            })
+                            // fim add gols t2
+                      
+                            //adicionar cartões amarelos time 2
+                            var container_ca_t2 = $(".container-ca-t2{{$game->id}}");
+                            var add_button_cat2 = $(".add_ca_t2{{$game->id}}");
+                            var x_cat2 = 1;
+                            $(add_button_cat2).click(function(e) {
+                                e.preventDefault();
+                                x_cat2++;
+                                $(container_ca_t2).append(`
+                                  <div class="row mt-2">
+                                      <div class="col-10">
+                                          <select class="form-select" id="team2_yellowcard" name="team2_yellowcard[]" aria-label="Default select example">
+                                              <option selected value="futebol">Selecione o Jogador</option>
+                                              @foreach ( $game->team2->players as $playerr)
+                                              <option value="{{$playerr->id}}">{{$playerr->name}}</option>
+                                              @endforeach
+                                          </select>
+                                      </div>
+                                      <a class="btn btn-sm btn-primary delete col-2"><i class="fa-solid fa-minus"></i></a>
+                                  </div>
+                                  `); //add input box
+                            });
+                      
+                            $(container_ca_t2).on("click", ".delete", function(e) {
+                                e.preventDefault();
+                                $(this).parent('div').remove();
+                                x_cat2--;
+                            })
+                            // fim add ca t2
+                      
+                            //adicionar cartões vermelhos time 2
+                            var container_cv_t2 = $(".container-cv-t2{{$game->id}}");
+                            var add_button_cvt2 = $(".add_cv_t2{{$game->id}}");
+                            var x_cvt2 = 1;
+                            $(add_button_cvt2).click(function(e) {
+                                e.preventDefault();
+                                x_cvt2++;
+                                $(container_cv_t2).append(`
+                                  <div class="row mt-2">
+                                      <div class="col-10">
+                                          <select class="form-select" id="team2_redcard" name="team2_redcard[]" aria-label="Default select example">
+                                              <option selected value="futebol">Selecione o Jogador</option>
+                                              @foreach ( $game->team2->players as $playerr)
+                                              <option value="{{$playerr->id}}">{{$playerr->name}}</option>
+                                              @endforeach
+                                          </select>
+                                      </div>
+                                      <a class="btn btn-sm btn-primary delete col-2"><i class="fa-solid fa-minus"></i></a>
+                                  </div>
+                                  `); //add input box
+                            });
+                      
+                            $(container_cv_t2).on("click", ".delete", function(e) {
+                                e.preventDefault();
+                                $(this).parent('div').remove();
+                                x_cavt2--;
+                            })
+                            // fim add cv t2
+                      
+                        });
+                      </script>
                       
                       @endforeach
 
