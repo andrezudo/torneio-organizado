25f4c1d18792d15c949e352c75e572c7dbf98e43
André Luis <aluis7649@gmail.com>
2022-06-29 00:20:52 -0300

    Consulta dos artilheiros do campeonato #25

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Statistic;
 
 class StatisticController extends Controller
 {
-    //
+    public function statistics($id) {
+
+        //SELECT player_id,COUNT(`player_id`) AS qtd FROM `statistics` WHERE `type` = 'gol' GROUP BY player_id ORDER BY qtd DESC;
+        $gols = Statistic::selectRaw('count(*) as qtd_gols, player_id')
+                ->with('player')
+                ->where('championship_id', '=', $id)
+                ->groupBy('player_id')
+                ->limit(3)
+                ->get();
+
+        //echo $gols;
+        return view('app.rankings', ['gols' => $gols]);
+    }
 }
