dffd15e2b8558d0ac14d9d0b26e8cd8c523352e3
André Luis <aluis7649@gmail.com>
2022-07-07 01:19:26 -0300

    Estatísticas na página do campeonato #26

--- app/Http/Controllers/ChampionshipController.php
+++ app/Http/Controllers/ChampionshipController.php
@@ -7,6 +7,7 @@ use App\Models\Championship;
 use App\Models\Team;
 use App\Models\Game;
 use App\Models\Table;
+use App\Models\Statistic;
 
 class ChampionshipController extends Controller
 {
@@ -44,8 +45,38 @@ class ChampionshipController extends Controller
                     ->orderBy('sg', 'desc')
                     ->orderBy('id', 'asc')
                     ->get();
+        $gols = Statistic::selectRaw('count(*) as qtd_gols, player_id')
+                    ->with('player')
+                    ->where('championship_id', '=', $id)
+                    ->where('type', '=', 'gol')
+                    ->groupBy('player_id')
+                    ->orderByDesc('qtd_gols')
+                    ->get();
+        $amarelos = Statistic::selectRaw('count(*) as qtd_amarelos, player_id')
+                    ->with('player')
+                    ->where('championship_id', '=', $id)
+                    ->where('type', '=', 'amarelo')
+                    ->groupBy('player_id')
+                    ->orderByDesc('qtd_amarelos')
+                    ->get();
+        $vermelhos = Statistic::selectRaw('count(*) as qtd_vermelhos, player_id')
+                    ->with('player')
+                    ->where('championship_id', '=', $id)
+                    ->where('type', '=', 'vermelho')
+                    ->groupBy('player_id')
+                    ->orderByDesc('qtd_vermelhos')
+                    ->get();
 
-        return view('site.campeonato', ['championship' => $championship, 'teams' => $teams, 'games' => $games, 'tables' => $tables]);
+        return view('site.campeonato', 
+                    [
+                        'championship' => $championship, 
+                        'teams' => $teams, 
+                        'games' => $games, 
+                        'tables' => $tables, 
+                        'gols' => $gols, 
+                        'amarelos' => $amarelos, 
+                        'vermelhos' => $vermelhos
+                    ]);
     }
 
     public function store(Request $request) {
--- resources/views/site/campeonato.blade.php
+++ resources/views/site/campeonato.blade.php
@@ -6,8 +6,8 @@
 
     <header>
 
-        <section class="mt-5 mb-5 text-white">
-            <div class="container mb-5">
+        <section class="mt-5 text-white">
+            <div class="container">
                 
                 <div class="">
                     <h2>{{ $championship->title }}</h2>
@@ -15,7 +15,7 @@
                     <p><i class="fa-solid fa-dollar-sign"></i> Premiação: R${{ $championship->award }},00</p>
                 </div>
 
-                <div class="container">
+                <div class="container mb-5">
                     <div class="mt-5 mb-2">
                         <h3>Times:</h3>
                     </div>
@@ -71,104 +71,160 @@
                     </div>
 
                     <div class="card-rankings p-2">
-                    <hr>
-                    @foreach ($games as $game)
-                        <div class="text-center d-flex justify-content-around">
-                            <div class="d-flex justify-content-evenly w-75">
-                                <div class="text-center w-25">
-                                    <p>{{$game->team1->name}} <i class="fa-solid fa-shield-halved"></i></p>
-                                </div>
-                                <div class="d-flex justify-content-evenly">
-                                    <div class="mx-2">
-                                        @if ( $game->result == null)
-                                            <p><span><b></b> </span></p> 
-                                        @else
-                                            <p><span><b>{{$game->result->team1_goals}}</b> </span></p>
-                                        @endif
+                        <hr>
+                        @foreach ($games as $game)
+                            <div class="text-center d-flex justify-content-around">
+                                <div class="d-flex justify-content-evenly w-75">
+                                    <div class="text-center w-25">
+                                        <p>{{$game->team1->name}} <i class="fa-solid fa-shield-halved"></i></p>
                                     </div>
-                                    <div class="mx-2">
-                                        <p><span> X </span></p>
+                                    <div class="d-flex justify-content-evenly">
+                                        <div class="mx-2">
+                                            @if ( $game->result == null)
+                                                <p><span><b></b> </span></p> 
+                                            @else
+                                                <p><span><b>{{$game->result->team1_goals}}</b> </span></p>
+                                            @endif
+                                        </div>
+                                        <div class="mx-2">
+                                            <p><span> X </span></p>
+                                        </div>
+                                        <div class="mx-2">
+                                            @if ( $game->result == null)
+                                                <P> <span><b></b></span></P>
+                                            @else
+                                                <P><span><b> {{$game->result->team2_goals}}</b></span></P>
+                                            @endif
+                                        </div>
                                     </div>
-                                    <div class="mx-2">
-                                        @if ( $game->result == null)
-                                            <P> <span><b></b></span></P>
-                                        @else
-                                            <P><span><b> {{$game->result->team2_goals}}</b></span></P>
-                                        @endif
+                                    <div class="text-center w-25">
+                                        <p><i class="fa-solid fa-shield-halved"></i> {{$game->team2->name}}</p>
                                     </div>
-                                </div>
-                                <div class="text-center w-25">
-                                    <p><i class="fa-solid fa-shield-halved"></i> {{$game->team2->name}}</p>
                                 </div>
                             </div>
-                        </div>
-                        <hr>
-                    @endforeach
+                            <hr>
+                        @endforeach
                     </div>
-                    
+
+                    <div class="mt-5 mb-4">
+                        <h3>Estatísticas:</h3>
+                    </div>
+
+                    <div class="container mb-5">
+
+                        <div class="barra-navegacao">
+                            <ul class="nav nav-tabs mb-5" id="myTab" role="tablist">
+                                <li class="nav-item" role="presentation">
+                                  <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Artilheiros</button>
+                                </li>
+                                <li class="nav-item" role="presentation">
+                                  <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Cartões amarelos</button>
+                                </li>
+                                <li class="nav-item" role="presentation">
+                                  <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">Cartões vermelhos</button>
+                                </li>
+                            </ul>
+                        </div>
+        
+                            <div class="tab-content" id="myTabContent">
+        
+                                <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+                                    <div class="card-rankings p-2 m-3">
+                
+                                        @if ( $gols->count() == 0 )
+                                            <div class="col-12 mb-5 mt-5 text-white text-center">
+                                                <h4>Nenhum artilheiro por enquanto</h4>
+                                            </div>
+                                        @else
+                                            <div class="table-responsive">
+                                                <table class="table text-white">
+                                                    <thead>
+                                                    <tr>
+                                                        <th scope="col">Jogador</th>
+                                                        <th scope="col">Gols</th>
+                                                    </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        @foreach ($gols as $gol)
+                                                        <tr>
+                                                            <td>{{$gol->player->name}}</td>
+                                                            <td>{{$gol->qtd_gols}}</td>
+                                                        </tr>
+                                                        @endforeach
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        @endif
+                
+                                    </div>
+                                </div>
+        
+                                <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+                                    <div class="card-rankings p-2 m-3">
+                                        @if ( $amarelos->count() == 0 )
+                                            <div class="col-12 mb-5 mt-5 text-white text-center">
+                                                <h4>Nenhum amarelado por enquanto</h4>
+                                            </div>
+                                        @else
+                                            <div class="table-responsive">
+                                                <table class="table text-white">
+                                                    <thead>
+                                                    <tr>
+                                                        <th scope="col">Jogador</th>
+                                                        <th scope="col">Cartões amarelos</th>
+                                                    </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        @foreach ($amarelos as $amarelo)
+                                                        <tr>
+                                                            <td>{{$amarelo->player->name}}</td>
+                                                            <td>{{$amarelo->qtd_amarelos}}</td>
+                                                        </tr>
+                                                        @endforeach
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        @endif
+                                    </div>
+                                </div>
+        
+                                <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">
+                                    <div class="card card-rankings p-2 m-3">
+                                        @if ( $vermelhos->count() == 0 )
+                                            <div class="col-12 mb-5 mt-5 text-white text-center">
+                                                <h4>Nenhum vermelhado por enquanto</h4>
+                                            </div>
+                                        @else
+                                            <div class="table-responsive">
+                                                <table class="table text-white">
+                                                    <thead>
+                                                    <tr>
+                                                        <th scope="col">Jogador</th>
+                                                        <th scope="col">Cartões vermelhos</th>
+                                                    </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        @foreach ($vermelhos as $vermelho)
+                                                        <tr>
+                                                            <td>{{$vermelho->player->name}}</td>
+                                                            <td>{{$vermelho->qtd_vermelhos}}</td>
+                                                        </tr>
+                                                        @endforeach
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        @endif
+                                    </div>
+                                </div>
+        
+                            </div>
+        
+                    </div>
+
                 </div>
 
             </div>
         </section>
-
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Crie seu campeonato</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form action="/app/championship" method="POST">
-                        @csrf
-                        <div class="mb-3">
-                        <label for="title" class="form-label">Campeonato:</label>
-                        <input type="text" class="form-control" id="title" name="title" placeholder="Nome do Campeonato">
-                        </div>
-                        <div class="mb-3">
-                            <label for="localization" class="form-label">Local:</label>
-                            <input type="text" class="form-control" id="localization" name="localization" placeholder="Onde ocorrerá o Campeonato">
-                        </div>
-                        <div class="mb-3">
-                            <label for="modality" class="form-label">Modalidade:</label>
-                            <select class="form-select" id="modality" name="modality" aria-label="Default select example">
-                                <option selected value="futebol">Futebol</option>
-                                <option value="futsal">Futsal</option>
-                                <option value="society">Society</option>
-                            </select>
-                        </div>
-                        <div class="mb-3 d-flex justify-content-center">
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="forma" id="mata_mata" value="1">
-                                <label class="form-check-label" for="mata_mata">Mata-mata</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="forma" id="groups" value="2">
-                                <label class="form-check-label" for="groups">Fase de grupos</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="forma" id="running_stitches" value="3">
-                                <label class="form-check-label" for="running_stitches">Pontos corridos</label>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="return" class="form-label">Ida e Volta:</label>
-                            <select class="form-select" id="return" name="return" aria-label="Default select example">
-                                <option selected value="1">Sim</option>
-                                <option value="0">Não</option>
-                            </select>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                            <button type="submit" class="btn btn-primary">Salvar</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            </div>
-        </div>
-
     </header>
 
 @endsection   
